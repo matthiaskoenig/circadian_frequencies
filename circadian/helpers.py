0db97a4556bac079dbc37a2b47509f67ac1782be
Matthias Koenig <konigmatt@googlemail.com>
2017-04-10 13:11:26 +0200

    frequency analysis tested

--- circadian/helpers.py
+++ circadian/helpers.py
@@ -6,6 +6,7 @@ from six import iteritems
 import numpy as np
 import scipy as sp
 import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot as plt
 import scipy.fftpack
 
@@ -39,13 +40,13 @@ def tf_data(z0_mu=1, z0_cv=0.1, phi_mu=0, phi_cv=0, T_mu=24, T_cv=0.01, sampling
     n_points = np.int(np.round(1.0 * duration / sampling))
     data = np.zeros(shape=(n_samples, n_points))
     for k in range(n_samples):
-        row = z0_mu + z0[k] * np.sin(2 * np.pi / T[k] * np.linspace(0, duration, n_points) + phi[k])
+        row = 1.2*z0_mu + z0[k] * np.sin(2 * np.pi / T[k] * np.linspace(0, duration, n_points) + phi[k])
         data[k, :] = row
 
     return data
 
 
-def fft_data(data):
+def fft_data(data, sampling):
     """ FFT of data to find frequencies.
     
     :param x: 
@@ -54,33 +55,72 @@ def fft_data(data):
     """
 
     n_samples, n_points = data.shape
-    yf = np.zeros(shape=(n_samples, n_points))
+    fft = np.zeros(shape=(n_samples, n_points))
+    yf = np.zeros(shape=(n_samples, int(np.round(n_points/2))) )
 
     for k in range(n_samples):
         # fft of row
         row = np.array(data[k, :])
-        yf[k, :] = scipy.fftpack.fft(row)
+        fft = scipy.fftpack.fft(row-np.mean(row))
+
+        # The returned complex array contains ``y(0), y(1), ..., y(n - 1)`` where
+        # y(j) = (x * exp(-2 * pi * sqrt(-1) * j * np.arange(n) / n)).sum()
+
+        yf[k, :] = 2.0 / n_points * np.abs(fft[:n_points // 2])
+
     return yf
+
+def full_dataset():
+    duration = 96
+    sampling = 1
+    n_samples = 100
+    n_points = np.int(np.round(1.0 * duration / sampling))
+
+
+    d1 = tf_data(sampling=1, duration=duration, n_samples=n_samples)
+    n_phis = 27
+    xf = np.linspace(0.0, 1.0 / (2.0 * sampling), n_points // 2)
+    phis = np.linspace(0, 360, n_phis)
+
+    spectrum = np.zeros(shape=(len(phis), n_points//2))
+
+    for k, phi in enumerate(phis):
+        d2 = tf_data(sampling=1, duration=duration, phi_mu=phi, n_samples=n_samples)
+        dsum = d1 + d2
+        yf = fft_data(dsum, sampling=sampling)
+        spectrum[k, :] =  np.mean(yf, axis=0)
+
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
+    for k, phi in enumerate(phis):
+        ax1.plot(xf, spectrum[k,:], alpha=0.5)
+
+    plt.show()
 
 
 if __name__ == "__main__":
-    duration = 48
+
+    # full_dataset()
+    duration = 96
     sampling = 1
-    n_points = np.int(np.round(1.0 * duration / sampling))
+    phi = 180
+    z0 = 1.0
+    T = 12
     n_samples = 100
+    n_points = np.int(np.round(1.0 * duration / sampling))
 
     d1 = tf_data(sampling=1, duration=duration, n_samples=n_samples)
-    d2 = tf_data(sampling=1, duration=duration, phi_mu=180, n_samples=n_samples)
-    dsum = d1+d2
+    d2 = tf_data(sampling=1, duration=duration, z0_mu=z0, phi_mu=phi, T_mu=T, n_samples=n_samples)
+    dsum = d1*d2
     datasets = [d1, d2, dsum]
 
     # x_fft = np.linspace(0.0, 1.0 / (2.0 * duration), int(np.round(n_points /2.0)))
 
     # ffts = [fft_data(data) for data in datasets]
-    ffts = [fft_data(data) for data in [d1]]
+    xf = np.linspace(0.0, 1.0 / (2.0 * sampling), n_points//2)
+    yfs = [fft_data(data, sampling=sampling) for data in [dsum]]
 
-
-    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(14, 7))
+    alpha = 0.3
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
     for d_index, data in enumerate(datasets):
         if d_index == 0:
             dcolor = 'blue'
@@ -90,21 +130,25 @@ if __name__ == "__main__":
             dcolor = 'black'
         print(d_index, dcolor)
         for k in range(n_samples):
-            ax1.plot(data[k, :], color=dcolor, alpha=0.5)
+            ax1.plot(data[k, :], color=dcolor, alpha=alpha)
 
-    for d_index, data in enumerate(ffts):
-        if d_index == 0:
-            dcolor = 'blue'
-        elif d_index == 1:
-            dcolor = 'darkgreen'
-        elif d_index == 2:
-            dcolor = 'black'
-        print(d_index, dcolor)
+    for d_index, data in enumerate(yfs):
+        dcolor='gray'
+
         for k in range(n_samples):
             # todo axis in frequencies
-            ax2.plot(data[k, :], color=dcolor, alpha=0.5)
+            ax2.plot(xf, data[k, :], '-o', color=dcolor, alpha=alpha)
+            ax3.plot(1/xf, data[k, :], '-o', color=dcolor, alpha=alpha)
+            # ax2.plot(1/xf, data[k, :], '-o', color=dcolor, alpha=alpha)
+        ax2.plot(xf, np.mean(data, axis=0), '-o', color='black', linewidth=2)
+        ax3.plot(1/xf, np.mean(data, axis=0), '-o', color='black', linewidth=2)
+
+        for xline in [8, 12, 24]:
+            ax3.plot([xline, xline], [0, 3.0], '--', color="black")
 
     ax1.set_ylabel('amplitude [a.u.]')
-    ax1.set_ylabel('time [h]')
+    ax1.set_xlabel('time [h]')
+    ax2.set_xlabel('frequency [1/h]')
+    ax3.set_xlabel('time [h]')
 
     plt.show()
